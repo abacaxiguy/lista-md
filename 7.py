150290de1468442622e327628e817428e8a548dd
João Lucas <Jlucasoc@hotmail.com>
2022-05-22 16:08:25 -0300

    Sétima questão ✅

---
+++ 7.py
@@ -0,0 +1,29 @@
+# inverso é o s unico
+# q esteja entre 0 e m, q é o b
+
+def combinacao_linear(a, b):
+    if b == 0:
+        s, t = 1, 0
+        return s, t
+
+    s, t = combinacao_linear(b, a % b)
+
+    return t, s - (a//b) * t
+
+
+a = int(input("Digite o a: "))
+b = int(input("Digite o b: "))
+
+cl = combinacao_linear(a, b)
+print(cl)
+
+
+def inverso(s, m):
+    if s > 0 and s < m:
+        return s
+
+    s %= m
+    return s
+
+
+print(f'inverso: {inverso(cl[0], b)}')
